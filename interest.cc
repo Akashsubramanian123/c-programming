29ec5ff66cdbee3d431c55689e336b5f69e53af8
Akashsubramanian123 <161033526+Akashsubramanian123@users.noreply.github.com>
2024-03-04 17:53:00 +0530

    interest.cc

--- interest.cc
+++ interest.cc
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-	float balance=2000,interest=0.2*balance;
-	printf("The balance is %0.2f\n",interest);
-	printf("The balance accumulated in 1 year:%0.2f\n",balance+interest);
+	float balance=1000,interest=0.2*balance;
+	printf("The interest accumulated in 1 year is %0.2f\n",interest);
+	printf("The balance accumulated in 1 year is %0.2f\n",interest+balance);
 	return 0;
 }
