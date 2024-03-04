93799b5009256ed1136b56e5d1545a1afc908f85
Akashsubramanian123 <161033526+Akashsubramanian123@users.noreply.github.com>
2024-03-04 17:36:25 +0530

    Grocery.cc

--- Grocery.cc
+++ Grocery.cc
@@ -1,32 +1,58 @@
 #include<stdio.h>
 int main()
 {
-	int apple=10,mango=15,amount=0,total=0,cash;
-	char fruits[20];
-	printf("Enter a fruits:\n");
-	scanf("%s",&fruits);
-	if(fruits=="apple"||fruits=="Apple")
+	int mango=20,apple=30,grapes=40,amount=0,total=0,cash;
+	char fruit[50],yes[50]="yes";
+	while (strcmp("yes",yes)==0)
 	{
-		printf("enter the amount of apple u want\n");
-		scanf("%d",&amount);
-		total+=(apple*amount);
+		printf("Enter the fruit u want:\n");
+		scanf("%s",fruit);
+		if(strcmp("mango",fruit)==0||strcmp("Mango",fruit)==0)
+		{
+			printf("Enter the amount of mango u want\n");
+			scanf("%d",&amount);
+			total+=(mango*amount);
+			printf("%s of %d kg is %d\n",fruit,amount,total);
+		}
+		else if(strcmp("apple",fruit)==0||strcmp("Apple",fruit)==0)
+		{
+			printf("Enter the amount of apple u want\n");
+			scanf("%d",&amount);
+			total+=(apple*amount);
+			printf("%s of %d kg is %d\n",fruit,amount,total);
+		}
+		else if(strcmp("grapes",fruit)==0||strcmp("Grapes",fruit)==0)
+		{
+			printf("Enter the amount of grapes u want\n");
+			scanf("%d",&amount);
+			total+=(grapes*amount);
+			printf("%s of %d kg is %d\n",fruit,amount,total);
+		}
+		else
+		{
+			printf("no such fruit available!");
+		}
+		printf("do you want to try again?\n");
+		scanf("%s",yes);
+		if (strcmp("yes",yes)==0||strcmp("Yes",yes)==0)
+		{
+			continue;
+		}
+		else
+		{
+			break;
+		}
 	}
-	else if(fruits=="mango"||fruits=="Mango")
-	{
-		printf("Enter the amount of mango u want\n");
-		scanf("%d",&amount);
-		total+=(mango*amount);
-	}
-	printf("total bill is %d",total);
-	printf("enter the cash u have\n");
-	scanf("%d",&cash);
-	if(cash>amount)
-	{
-		printf("the cash is %d\n",cash=total);
-    }
-	else
-	{
-		printf("insufficient balance\n");
-	}
+		printf("total bill is %d\n",total);
+		printf("Enter the cash you have\n");
+		scanf("%d",&cash);
+		if(cash>=amount)
+		{
+			printf("the balance is %d\n",cash-total);
+		}
+		else
+		{
+			printf("insufficient cash!\n");
+		}
 	return 0;
 }
